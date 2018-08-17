9f0ebb97465528631d22ea174ae1db6a0e42ac17
puku0x <puku0x@gmail.com>
2018-08-17 11:39:27 +0900

    fix

--- src/app/store/todo/todo-store.module.ts
+++ src/app/store/todo/todo-store.module.ts
@@ -2,13 +2,13 @@ import { NgModule} from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import { reducer, initialState } from './reducers';
+import { reducer } from './reducers';
 import { TodoEffects } from './effects';
 import { TodoFacade } from './todo.facade';
 
 @NgModule({
   imports: [
-    StoreModule.forFeature('todo', reducer, { initialState }),
+    StoreModule.forFeature('todo', reducer),
     EffectsModule.forFeature([TodoEffects])
   ],
   providers: [TodoFacade]
