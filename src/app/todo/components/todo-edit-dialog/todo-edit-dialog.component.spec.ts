1a74bd0fdd946747ac58cd36fd67d5af4732c2c8
puku0x <puku0x@gmail.com>
2018-09-10 21:05:30 +0900

    fix

--- src/app/todo/components/todo-edit-dialog/todo-edit-dialog.component.spec.ts
+++ src/app/todo/components/todo-edit-dialog/todo-edit-dialog.component.spec.ts
@@ -7,6 +7,7 @@ import {
  } from '@angular/material';
 
 import { TodoEditDialogComponent } from './todo-edit-dialog.component';
+import { Todo } from '@app/models';
 
 describe('TodoEditDialogComponent', () => {
   let component: TodoEditDialogComponent;
@@ -25,10 +26,26 @@ describe('TodoEditDialogComponent', () => {
     .compileComponents();
     fixture = TestBed.createComponent(TodoEditDialogComponent);
     component = fixture.componentInstance;
+    spyOn(component.create, 'emit');
+    spyOn(component.update, 'emit');
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit create', () => {
+    component.todo = new Todo(null, 'test');
+    component.ngOnInit();
+    component.onSubmit();
+    expect(component.create.emit).toHaveBeenCalled();
+  });
+
+  it('should emit uodate', () => {
+    component.todo = new Todo('1', 'test');
+    component.ngOnInit();
+    component.onSubmit();
+    expect(component.update.emit).toHaveBeenCalled();
   });
 
 });
