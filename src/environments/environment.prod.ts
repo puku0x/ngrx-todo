1812a8307d4d6f1640f1a056d4996a67ed69c7a7
puku0x <puku0x@gmail.com>
2019-08-10 18:46:38 +0900

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://us-central1-todo-api-4119c.cloudfunctions.net/v1'
+  baseUrl: 'https://us-central1-todo-api-4119c.cloudfunctions.net/api'
 };
