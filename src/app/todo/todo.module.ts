d8a38fe8de31da4d98b1fce6c7ba9e8b9364b79c
puku0x <puku0x@gmail.com>
2021-01-17 17:05:32 +0900

    remove material module

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
-import { MaterialModule } from '../material';
 import { TodoListComponent, TodoListItemComponent } from './components';
 import {
   TodoCreateDialogComponent,
@@ -18,7 +23,12 @@ import { TodoComponent } from './todo.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
     TodoRoutingModule,
     TodoStoreModule,
   ],
